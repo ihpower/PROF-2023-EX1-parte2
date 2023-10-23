dcbf5fab7b0c7d92ca068eac8c9c007192f06e3b
Sergio de Lamadrid <s.lamadrid2007@gmail.com>
2023-10-23 18:41:15 +0200

    Añadida Test Suite

---
+++ src/test/java/ClientePedidoTestSuite.java
@@ -0,0 +1,10 @@
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectClasses({ClienteTest.class, PedidoTest.class})
+public class ClientePedidoTestSuite {
+    
+}
