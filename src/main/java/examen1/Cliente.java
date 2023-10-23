aafacfb825cb6b12119ad9aaf406f7e9e6b83a4d
LuisBMI <luisbmi55@gmail.com>
2023-10-23 18:17:27 +0200

    Build success

---
+++ src/main/java/examen1/Cliente.java
@@ -0,0 +1,32 @@
+package examen1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cliente {
+	
+	private List<Pedido> pedidos;
+	
+	Cliente() {
+		pedidos = new ArrayList<Pedido>();
+	}
+	
+	public void setPedido(Pedido pedido) {
+		pedidos.add(pedido);
+	}
+
+	public double calculaImportePedidos() throws NoHayPedidosException {
+		
+		double importe = 0;
+		
+		if(pedidos.isEmpty())
+			throw new NoHayPedidosException();
+		
+		for(Pedido i : pedidos) {
+			importe += i.getImportePedido();
+		}
+		
+		return importe;
+	}
+	
+}
---
+++ src/main/java/examen1/NoHayPedidosException.java
@@ -0,0 +1,7 @@
+package examen1;
+
+public class NoHayPedidosException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+}
