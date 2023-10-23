c54f1fd1a2ba5ca34c7dadde2d9c1b7178dce8ed
Samuel Dámaso <sdamasodemiguel@gmail.com>
2023-10-23 18:49:43 +0200

    Creación de Suite de pruebas

---
+++ src/test/java/com/examen/PedidoClienteSuite.java
@@ -0,0 +1,11 @@
+package com.examen;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectClasses({ PedidoTest.class, ClienteTest.class })
+public class PedidoClienteSuite {
+
+}
